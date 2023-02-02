4c4cf364c0ea2d41ee389368740090bfa17fb624
Reynaldo Aponte <playrey2010@gmail.com>
2023-02-02 13:16:02 -0800

    Working on Progress Bar

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Mercedes Martinez Milantchi on 2/2/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+//    question as a struct
+    struct Question {
+        let text: String
+        let answer: String
+        
+        init(q: String, a: String){
+            text = q
+            answer = a
+        }
+    }
+
+
+
